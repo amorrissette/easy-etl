49f257a3763f772e98e73f423615b1af223f8103
Arthur Morrissette <arthur.morrissette@gmail.com>
2024-09-24 18:20:41 -0400

    fix format/layout

--- app/page.tsx
+++ app/page.tsx
@@ -44,8 +44,8 @@ export default function Home() {
         {!csvData && <Dropzone onCsvData={setCsvData} />}
       </div>
 
-      {(!csvData && <div className="w-full items-center justify-center font-mono text-md pt-2 pb-2">
-        AI inference is currently supported by <a href="https://groq.com/" target="_blank" rel="noopener noreferrer">Groq</a>. Easy ETL and Groq do not store any user data - <a href="https://groq.com/privacy-policy/" target="_blank" rel="noopener noreferrer">Learn more.</a>
+      {(!csvData && <div className="flex w-full items-center justify-center font-mono text-md pt-2 pb-2 italic">
+        <p>AI inference is currently supported by <a href="https://groq.com/" target="_blank" rel="noopener noreferrer" style={{fontWeight: 'bold', whiteSpace: 'nowrap'}}>Groq</a>. Easy ETL and Groq do not store any user data - <a href="https://groq.com/privacy-policy/" target="_blank" rel="noopener noreferrer" style={{fontWeight: 'bold', whiteSpace: 'nowrap'}}>Learn more.</a></p>
       </div>)}
 
       <div className="w-full pt-8">
